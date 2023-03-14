6c0e36e4e53222ced252d2bfe177ca72104a3174
Ankit Marwaha <ankit.marwaha@nagarro.com>
2023-03-14 10:41:46 +0530

    setup updated

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import find_packages,setup
 from typing import List
 
-def get_requirements()->List[str]:
-    """
-    This function will return list of requirements
-    """
-    requirement_list:List[str] = []
-    with open('requirements.txt', 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            requirement_list.append(line.strip())
-    return requirement_list
+# def get_requirements()->List[str]:
+#     """
+#     This function will return list of requirements
+#     """
+#     requirement_list:List[str] = []
+#     with open('requirements.txt', 'r') as f:
+#         lines = f.readlines()
+#         for line in lines:
+#             requirement_list.append(line.strip())
+#     return requirement_list
 
 setup(
     name="sensor",
     version="0.0.1",
     author="Ankit Marwaha",
     author_email="ankitmarwaha8@gmail.com",
-    packages = find_packages(),
-    install_requires=get_requirements()#["pymongo==4.2.0"],
+    packages = find_packages()
+    #install_requires=get_requirements()#["pymongo==4.2.0"],
 )
