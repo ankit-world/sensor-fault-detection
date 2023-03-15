bc80874ca5cf28a527d20bddd735f417a6b51118
Ankit Marwaha <ankit.marwaha@nagarro.com>
2023-03-15 11:43:25 +0530

    data ingestion code updated

---
+++ main.py
@@ -0,0 +1,11 @@
+from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.exception import SensorException
+import os,sys
+from sensor.logger import logging
+from sensor.pipeline import training_pipeline
+from sensor.pipeline.training_pipeline import TrainPipeline
+from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
+
+if __name__ == '__main__':
+    training_pipeline = TrainPipeline()
+    training_pipeline.run_pipeline()
